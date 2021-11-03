83fc72d94dc07b6fd2f00a33cda16e308dd7fd54
dehaon2 <vortex_power@mail.ru>
2021-11-04 00:11:19 +0700

    lyaaaasss22dsad123ddsa

---
+++ app/router — копия.py
@@ -0,0 +1,12 @@
+class Router:
+    def __init__(self, app):
+        self.app = app
+
+    def routes(self):
+        @self.app.route("/")
+        def hello_world():
+            return "<p>Hello, vladiksdsdsdsddas123123asdasd!</p>"
+
+        @self.app.route("/heh")
+        def hello_world():
+            return "<p>mda</p>"
