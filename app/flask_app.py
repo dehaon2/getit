5e64b2f4f45c69ab2707243078c5873af2fc0da2
dehaon2 <vortex_power@mail.ru>
2021-11-03 23:04:13 +0700

    lyaaaasss22

--- app/flask_app.py
+++ app/flask_app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, vladiksdsd!</p>"
+    return "<p>Hello, vladiksdsdsd!</p>"
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
